785558a71666ba0ff34673b9973947d3690f5a64
Alex Saunders <acsauk@gmail.com>
2017-07-20 13:29:17 +0100

    creates failing test for property validation

--- spec/models/recipe_spec.rb
+++ spec/models/recipe_spec.rb
@@ -3,8 +3,11 @@ require 'rails_helper'
 RSpec.describe Recipe, type: :model do
 
   let(:recipe) { FactoryGirl.build(:recipe) }
-  
+
   it 'has a valid factory' do
     expect(recipe).to be_valid
   end
+
+  it { is_expected.to validate_presence_of(:title) }
+  it { is_expected.to validate_presence_of(:instructions) }
 end
