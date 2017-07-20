54b6f5f25d0cb74bd30c8722608bf3dbf372b7a8
Alex Saunders <acsauk@gmail.com>
2017-07-20 12:16:55 +0100

    switches to false for fixture transactions to enable database_cleaner. Creates factories folder and initial recipe factory

---
+++ spec/factories/recipes.rb
@@ -0,0 +1,10 @@
+FactoryGirl.define do
+  factory :recipe do
+    sequence :title do |n|
+      "Title text #{n}"
+    end
+    sequence :instructions do |n|
+      "Instructions text #{n}"
+    end
+  end
+end
