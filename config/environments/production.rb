d4c26f186432ba3873b1ac20f497fb0003d7eb01
Alex Saunders <acsauk@gmail.com>
2017-07-24 10:07:29 +0100

    installs devise via rails generator, adds default mailer url option to development and adds commented out version to production env. Adds flash messages to application view and converts to haml, generates devise views ready to customise when required

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -88,4 +88,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  # Configure below when pushing to production for devise
+  # config.action_mailer.default_url_options = { host: 'xxxxxx', port: 0000 }
 end
