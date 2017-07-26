71f2b2554db58d23733e3fc7c94090e1f9a45585
Alex Saunders <acsauk@gmail.com>
2017-07-26 12:36:54 +0100

    creates and passes new test for conditional user links

--- spec/features/users_feature_spec.rb
+++ spec/features/users_feature_spec.rb
@@ -1,18 +1,25 @@
 feature 'User Management' do
   scenario 'Shows a welcome page when navigation to /' do
     visit '/'
-
     expect(page).to have_content('Welcome to Food Truck Hero!!')
   end
 
-  scenario 'I can sign up as a new user' do
-    visit '/'
-    click_link 'Sign up'
-    fill_in :user_email, with: 'test_email@gmail.com'
-    fill_in :user_password, with: 'Password123'
-    fill_in :user_password_confirmation, with: 'Password123'
-    click_button 'Sign up'
+  context 'when not signed in' do
+    scenario 'I can sign up as a new user' do
+      visit '/'
+      click_link 'Sign up'
+      fill_in :user_email, with: 'test_email@gmail.com'
+      fill_in :user_password, with: 'Password123'
+      fill_in :user_password_confirmation, with: 'Password123'
+      click_button 'Sign up'
 
-    expect(page).to have_content(I18n.t('devise.registrations.signed_up'))
+      expect(page).to have_content(I18n.t('devise.registrations.signed_up'))
+    end
+
+    scenario 'I can see sign in/up links' do
+      visit '/'
+      expect(page).to have_content('Sign in')
+      expect(page).to have_content('Sign up')
+    end
   end
 end
