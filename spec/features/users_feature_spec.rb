f83b49eb42342b51f8ae49731b120b90b10a4e7e
Alex Saunders <acsauk@gmail.com>
2017-07-27 10:05:07 +0100

    creates and passes test for signed in users seeing the correct user links

--- spec/features/users_feature_spec.rb
+++ spec/features/users_feature_spec.rb
@@ -5,6 +5,10 @@ feature 'User Management' do
   end
 
   context 'when not signed in' do
+    before(:each) do
+      visit '/'
+    end
+
     scenario 'I can sign up as a new user' do
       sign_up
 
@@ -12,25 +16,47 @@ feature 'User Management' do
     end
 
     scenario 'I can sign in as an existing user' do
-      sign_up
+      sign_up(email: 'test2@gmail.com')
       click_link 'Sign out'
       click_link 'Sign in'
-      fill_in :user_email, with: 'test_email@gmail.com'
-      fill_in :user_password, with: 'Password123'
+      fill_in :user_email, with: 'test2@gmail.com'
+      fill_in :user_password, with: 'password123'
       click_button 'Log in'
 
       expect(page).to have_content(I18n.t('devise.sessions.signed_in'))
     end
 
     scenario 'I can see sign in/up links' do
-      visit '/'
+      # visit '/'
       expect(page).to have_link('Sign in')
       expect(page).to have_link('Sign up')
     end
 
     scenario 'I cannot see sign out link' do
-      visit '/'
+      # visit '/'
       expect(page).to have_no_link('Sign out')
+    end
+  end
+
+  context 'when signed in' do
+    before(:each) do
+      visit '/'
+      sign_up
+      click_link 'Sign in'
+      fill_in :user_email, with: 'test@gmail.com'
+      fill_in :user_password, with: 'password123'
+      click_button 'Log in'
+    end
+
+    scenario 'I cannot see sign in/up links' do
+      # visit '/'
+      expect(page).to have_no_link('Sign in')
+      expect(page).to have_no_link('Sign up')
+    end
+
+    scenario 'I can see sign out link' do
+      # visit '/'
+      expect(page).to have_link('Sign out')
     end
   end
 end
--- spec/support/users_helper.rb
+++ spec/support/users_helper.rb
@@ -1,8 +1,8 @@
 def sign_up(email: 'test@gmail.com', password: 'password123')
   visit '/'
   click_link 'Sign up'
-  fill_in :user_email, with: 'test_email@gmail.com'
-  fill_in :user_password, with: 'Password123'
-  fill_in :user_password_confirmation, with: 'Password123'
+  fill_in :user_email, with: email
+  fill_in :user_password, with: password
+  fill_in :user_password_confirmation, with: password
   click_button 'Sign up'
 end
