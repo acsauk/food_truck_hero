e54cc450d433873521614b64833678e2b0453757
Alex Saunders <acsauk@gmail.com>
2017-08-31 10:44:10 +0100

    adds validations for name, amount and unit in ingredient model and creates and passes associated spec

--- spec/models/ingredient_spec.rb
+++ spec/models/ingredient_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe Ingredient, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:ingredient) { FactoryGirl.build(:ingredient) }
+
+  it 'has a valid factory' do
+    expect(ingredient).to be_valid
+  end
+
+  it { is_expected.to validate_presence_of(:name) }
+  it { is_expected.to validate_presence_of(:amount) }
+  it { is_expected.to validate_presence_of(:unit) }
 end
