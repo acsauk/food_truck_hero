ef9082bf0a1f1f7c3715c42f11c056849d9d8754
Alex Saunders <acsauk@gmail.com>
2017-08-31 10:40:38 +0100

    creates model and migration for ingredients, carries out migration

---
+++ db/migrate/20170831093714_create_ingredients.rb
@@ -0,0 +1,11 @@
+class CreateIngredients < ActiveRecord::Migration[5.1]
+  def change
+    create_table :ingredients do |t|
+      t.string :name,   null: false
+      t.string :amount, null: false
+      t.float :unit,    null: false
+
+      t.timestamps
+    end
+  end
+end
---
+++ spec/factories/ingredients.rb
@@ -0,0 +1,7 @@
+FactoryGirl.define do
+  factory :ingredient do
+    name "MyString"
+    amount "MyString"
+    unit 1.5
+  end
+end
