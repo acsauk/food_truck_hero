935307710b7319b2fff20e96a7b7884eb7ec8203
Alex Saunders <acsauk@gmail.com>
2017-08-31 10:33:55 +0100

    builds out recipe spec to a point where an ingredient model is required

--- spec/features/recipe_feature_spec.rb
+++ spec/features/recipe_feature_spec.rb
@@ -4,15 +4,16 @@ feature 'Recipes' do
     visit '/'
     sign_up
     save_and_open_page
-    click_link 'Add Recipe'
+    click_link 'New Recipe'
     fill_in 'Title', with: 'Recipe title'
     fill_in 'Instructions', with: 'Recipe instructions'
-    fill_in '#ingredient-1', with: 'Ingredient name'
-    fill_in '#quantity-1', with: 'Quantity amount'
-    fill_in '#unit-1', with: 'Unit type'
-    click_button 'Create recipe'
-    expect(page).to have_content 'Recipe title recipe successfully created'
-    expect(page).to have_link 'Recipe title'
-    expect(current_path).to eq '/recipes'
+    fill_in 'ingredient_1', with: 'Ingredient name'
+    fill_in 'quantity_1', with: 'Quantity amount'
+    fill_in 'unit_1', with: 'Unit type'
+    click_button 'Create Recipe'
+    expect(page).to have_content 'Recipe was successfully created'
+    expect(page).to have_content 'Title: Recipe title'
+    expect(page).to have_content 'Instructions: Recipe instructions'
+    expect(page).to have_content 'Ingredients: Ingredient name Quantity amount Unit type'
   end
 end
