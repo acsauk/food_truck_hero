568ec7259fac6e47e7a58b38eb214bc3c239a630
Alex Saunders <acsauk@gmail.com>
2017-09-01 10:06:33 +0100

    whitelists ingredients attributes - still being rejected when sent

--- app/models/ingredient_list.rb
+++ app/models/ingredient_list.rb
@@ -1,5 +1,5 @@
 class IngredientList < ApplicationRecord
   belongs_to :recipe
   belongs_to :ingredient
-  accepts_nested_attributes_for :ingredient, allow_destroy: :true
+  # accepts_nested_attributes_for :ingredient, allow_destroy: :true
 end
--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -2,5 +2,6 @@ class Recipe < ApplicationRecord
   has_many :ingredientLists
   has_many :ingredients, through: :ingredientLists
   validates_presence_of :title, :instructions
-  accepts_nested_attributes_for :ingredientLists, allow_destroy: :true
+  accepts_nested_attributes_for :ingredientLists, allow_destroy: :true,
+                                                  reject_if: :all_blank
 end
