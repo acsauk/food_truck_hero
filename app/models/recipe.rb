f0c32df582140129996073aaa91e9c66a437693f
Alex Saunders <acsauk@gmail.com>
2018-01-23 22:44:01 +0000

    reinstates validations to models

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -3,4 +3,6 @@ class Recipe < ApplicationRecord
   has_many :ingredients, through: :ingredientLists
 
   accepts_nested_attributes_for :ingredientLists
+
+  validates_presence_of :instructions, :title
 end
