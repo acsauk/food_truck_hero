f0e92b0c64c77b6e8933274b4c56abfba07eb99f
Alex Saunders <acsauk@gmail.com>
2018-01-27 16:13:57 +0000

    updates model validations, views and controllers to take into account new structure of dbs/models

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -2,5 +2,5 @@ class Ingredient < ApplicationRecord
   has_many :ingredientLists
   has_many :recipes, through: :ingredientLists
 
-  validates_presence_of :name, :unit, :amount
+  validates_presence_of :name
 end
--- app/models/ingredient_list.rb
+++ app/models/ingredient_list.rb
@@ -2,12 +2,11 @@ class IngredientList < ApplicationRecord
   belongs_to :recipe, optional: true
   belongs_to :ingredient, optional: true
 
+  validates_presence_of :unit, :amount
+
   def ingredient_attributes=(ingredient_attributes)
     if ingredient_attributes['name']
-      new_ingredient = Ingredient.find_or_create_by(name: ingredient_attributes['name']) do |i|
-        i.amount = ingredient_attributes['amount']
-        i.unit = ingredient_attributes['unit']
-      end
+      new_ingredient = Ingredient.find_or_create_by(name: ingredient_attributes['name'])
       self.ingredient = new_ingredient
     end
   end
