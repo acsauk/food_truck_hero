c8ed4cd1c1c8ddd680eea6f05e86b7cbe735b3d0
Alex Saunders <acsauk@gmail.com>
2018-01-29 22:46:00 +0000

    creates failing test for deleting ingredientLists associate when deleting recipe

--- spec/models/recipe_spec.rb
+++ spec/models/recipe_spec.rb
@@ -2,7 +2,8 @@ require 'rails_helper'
 
 RSpec.describe Recipe, type: :model do
 
-  let(:recipe) { FactoryBot.build(:recipe) }
+  let(:recipe) { FactoryBot.create(:recipe) }
+  let(:rwi) { FactoryBot.create(:recipe_with_ingredients) }
 
   it 'has a valid factory' do
     expect(recipe).to be_valid
@@ -13,4 +14,9 @@ RSpec.describe Recipe, type: :model do
   it { is_expected.to have_many(:ingredients).through(:ingredientLists) }
   it { is_expected.to have_many(:ingredientLists) }
   it { is_expected.to accept_nested_attributes_for(:ingredientLists) }
+
+  it 'deletes associated IngredientLists when deleted' do
+    rwi.delete
+    expect(rwi.ingredientLists.size).to eql(0)
+  end
 end
