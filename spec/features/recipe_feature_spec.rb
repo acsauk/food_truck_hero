001da3118a7821b8795fb2cdf2221c37e4c11fab
Alex Saunders <acsauk@gmail.com>
2018-02-05 22:32:06 +0000

    updates form view to cycle through the amount of fields produced on the page and changes tests to match

--- spec/features/recipe_feature_spec.rb
+++ spec/features/recipe_feature_spec.rb
@@ -1,36 +1,12 @@
 feature 'Recipes' do
-  scenario 'adding recipes - single ingredient' do
-    visit '/'
-    sign_up
-    click_link 'New Recipe'
-    fill_in 'Title', with: 'Recipe title'
-    fill_in 'Instructions', with: 'Recipe instructions'
-    fill_in 'name', with: 'Ingredient name'
-    fill_in 'amount', with: '20'
-    fill_in 'unit', with: 'Ingredient unit'
-    click_button 'Create Recipe'
-    expect(page).to have_content 'Recipe was successfully created'
-    expect(page).to have_content 'Title: Recipe title'
-    expect(page).to have_content 'Instructions: Recipe instructions'
-    expect(page).to have_content 'Ingredients: Ingredient name 20.0 Ingredient unit'
-  end
-
   scenario 'adding recipes - multiple ingredients' do
     visit '/'
     sign_up
-    click_link 'New Recipe'
-    fill_in 'Title', with: 'Recipe title'
-    fill_in 'Instructions', with: 'Recipe instructions'
-    fill_in 'name', with: 'Ingredient name'
-    fill_in 'amount', with: '20'
-    fill_in 'unit', with: 'Ingredient unit'
-    fill_in 'name[2]', with: 'Ingredient name 2'
-    fill_in 'amount[2]', with: '21'
-    fill_in 'unit[2]', with: 'Ingredient unit 2'
+    create_recipe(ingredients_count: 2)
     expect(page).to have_content 'Recipe was successfully created'
     expect(page).to have_content 'Title: Recipe title'
     expect(page).to have_content 'Instructions: Recipe instructions'
-    expect(page).to have_content 'Ingredient name 20.0 Ingredient unit'
+    expect(page).to have_content 'Ingredient name 1 20.0 Ingredient unit 1'
     expect(page).to have_content 'Ingredient name 2 21.0 Ingredient unit 2'
   end
 end
