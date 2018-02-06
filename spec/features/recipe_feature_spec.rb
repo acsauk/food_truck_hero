953ceb7955a6032638e5611a0f67d2640acba16d
Alex Saunders <acsauk@gmail.com>
2018-02-06 20:33:14 +0000

    updates paths to use route names rather than absolute value, failing test for deleting recipes from recipe show

--- spec/features/recipe_feature_spec.rb
+++ spec/features/recipe_feature_spec.rb
@@ -1,6 +1,6 @@
 feature 'Recipes' do
   scenario 'adding recipes - multiple ingredients' do
-    visit '/'
+    visit root_path
     sign_up
     create_recipe(ingredients_count: 2)
     expect(page).to have_content 'Recipe was successfully created'
@@ -8,5 +8,14 @@ feature 'Recipes' do
     expect(page).to have_content 'Instructions: Recipe instructions'
     expect(page).to have_content 'Ingredient name 1 20.0 Ingredient unit 1'
     expect(page).to have_content 'Ingredient name 2 21.0 Ingredient unit 2'
+  end
+
+  scenario 'deleting recipes - recipe show view' do
+    visit root_path
+    sign_up
+    create_recipe(ingredients_count: 2)
+    visit recipes_path 1
+    click_link 'Delete'
+    expect(page).to have_content 'Recipe was successfully deleted'
   end
 end
