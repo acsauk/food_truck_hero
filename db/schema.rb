419336fb7c0ddd4497d885eacd8ef74033cee01c
Alex Saunders <acsauk@gmail.com>
2018-02-11 22:33:59 +0000

    creates and carries out migration to create product model and add product_id foreign key column to ingredients

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180211170840) do
+ActiveRecord::Schema.define(version: 20180211221650) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,6 +26,18 @@ ActiveRecord::Schema.define(version: 20180211170840) do
 
   create_table "ingredients", force: :cascade do |t|
     t.string "name", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.bigint "product_id"
+    t.index ["product_id"], name: "index_ingredients_on_product_id"
+  end
+
+  create_table "products", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "brand", null: false
+    t.string "unit", null: false
+    t.float "size", null: false
+    t.float "price", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
@@ -62,5 +74,6 @@ ActiveRecord::Schema.define(version: 20180211170840) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "ingredients", "products"
   add_foreign_key "recipes", "users", on_delete: :cascade
 end
---
+++ spec/factories/products.rb
@@ -0,0 +1,9 @@
+FactoryBot.define do
+  factory :product do
+    name "MyString"
+    brand "MyString"
+    unit "MyString"
+    size 1.5
+    price 1.5
+  end
+end
