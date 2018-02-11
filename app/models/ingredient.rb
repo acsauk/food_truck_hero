d8adf059990830e117cc206aab8f5968fb7332e5
Alex Saunders <acsauk@gmail.com>
2018-02-11 22:40:00 +0000

    adds associations between product and ingredient and validations to product

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -1,6 +1,7 @@
 class Ingredient < ApplicationRecord
   has_many :ingredientLists
   has_many :recipes, through: :ingredientLists
+  belongs_to :product
 
   validates_presence_of :name
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,8 @@
 class Product < ApplicationRecord
+  has_many :ingredients
+
+  validates_presence_of :name
+  validates_presence_of :brand
+  validates_presence_of :size
+  validates_presence_of :price
 end
