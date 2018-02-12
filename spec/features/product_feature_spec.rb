27e0e5ae4a3df910f42f575cc061c57edc6a398d
Alex Saunders <acsauk@gmail.com>
2018-02-12 22:00:00 +0000

    scaffolds Product, updates feature spec to reference correct fields

--- spec/features/product_feature_spec.rb
+++ spec/features/product_feature_spec.rb
@@ -6,15 +6,16 @@ feature 'Products' do
     visit root_path
     find("#nav_bar > a[href='#{products_path}']").click
     find("a[href='#{new_product_path}']").click
-    fill_in :name, with: name
-    fill_in :brand, with: brand
-    fill_in :brand, with: unit
-    fill_in :amount, with: amount
-    fill_in :price, with: price
+    fill_in :product_name, with: 'Product Name'
+    fill_in :product_brand, with: 'Product Brand'
+    fill_in :product_unit, with: 'grams'
+    fill_in :product_amount, with: 2000
+    fill_in :product_price, with: 20
     click_button 'Create Product'
     expect(page).to have_content 'Product was successfully created'
     expect(page).to have_content 'Name: Product Name'
     expect(page).to have_content 'Brand: Product Brand'
+    expect(page).to have_content 'Price: £20'
     expect(page).to have_content 'Amount: 2000 grams'
   end
 end
