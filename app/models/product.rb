97585227e72332b7102b264dc69bd3b7016f3cad
Alex Saunders <acsauk@gmail.com>
2018-02-13 21:14:05 +0000

    updates migration file and migrates, removes ingredient association from product

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,4 @@
 class Product < ApplicationRecord
-  has_many :ingredients
-
   validates_presence_of :name
   validates_presence_of :brand
   validates_presence_of :unit
--- db/migrate/20180211221650_create_products_and_product_lists.rb
+++ db/migrate/20180211221650_create_products_and_product_lists.rb
@@ -1,4 +1,4 @@
-class CreateProducts < ActiveRecord::Migration[5.1]
+class CreateProductsAndProductLists < ActiveRecord::Migration[5.1]
   def change
     create_table :products do |t|
       t.string :name, null: false
@@ -6,6 +6,13 @@ class CreateProducts < ActiveRecord::Migration[5.1]
       t.string :unit, null: false
       t.float :amount, null: false
       t.float :price, null: false
+
+      t.timestamps
+    end
+
+    create_table :product_lists do |t|
+      t.integer :product_id, null: false, index: true
+      t.integer :meal_id,    null: false, index: true
 
       t.timestamps
     end
