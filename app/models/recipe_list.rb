e4a57d9ab808b95e5908f7b470957f5b89130fb0
Alex Saunders <acsauk@gmail.com>
2018-02-18 18:49:22 +0000

    creates and passes spec for recipeList model

--- app/models/recipe_list.rb
+++ app/models/recipe_list.rb
@@ -1,2 +1,4 @@
 class RecipeList < ApplicationRecord
+  belongs_to :meal
+  belongs_to :recipe
 end
--- spec/models/recipe_list_spec.rb
+++ spec/models/recipe_list_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe RecipeList, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:recipe) { FactoryBot.build(:recipe) }
+  let(:recipeList) { FactoryBot.build(:recipeList) }
+  let(:meal) { FactoryBot.build(:meal) }
+
+  it 'has a valid factory' do
+    expect(ingredientList).to be_valid
+  end
+
+  it { is_expected.to belong_to(:recipe) }
+  it { is_expected.to belong_to(:meal) }
 end
