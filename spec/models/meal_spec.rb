0a37a67e0ba800c14e97c2b7cd010baa968e4c76
Alex Saunders <acsauk@gmail.com>
2018-02-18 15:56:55 +0000

    adds additional assertions to meal_spec

--- spec/models/meal_spec.rb
+++ spec/models/meal_spec.rb
@@ -1,9 +1,11 @@
 require 'rails_helper'
-
 RSpec.describe Meal, type: :model do
   let(:meal) { FactoryBot.create(:meal) }
 
   it 'has a valid factory' do
     expect(recipe).to be_valid
   end
+
+  it { is_expected.to validate_presence_of(:name) }
+  it { is_expected.to validate_presence_of(:yield) }
 end
