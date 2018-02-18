d98e4673a24446e97841bad4fe93b00e726a34ac
Alex Saunders <acsauk@gmail.com>
2018-02-18 18:33:31 +0000

    adds associations between recipe and meal model via recipeList, failing test for recipeList as it doesnt exist yet

--- app/models/meal.rb
+++ app/models/meal.rb
@@ -1,5 +1,8 @@
 class Meal < ApplicationRecord
-    validates_presence_of :name
-    validates_presence_of :portions
-    validates_presence_of :price_per_portion
+  has_many :recipeLists
+  has_many :recipes, through: :recipeLists
+
+  validates_presence_of :name
+  validates_presence_of :portions
+  validates_presence_of :price_per_portion
 end
