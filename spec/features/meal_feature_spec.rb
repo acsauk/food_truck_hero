5df549d5e83a3be6895da0607d6d78cd9a29cc18
Alex Saunders <acsauk@gmail.com>
2018-02-18 18:12:37 +0000

    updates creating a meal spec to test for the ingredients associated with recipe to be shown

--- spec/features/meal_feature_spec.rb
+++ spec/features/meal_feature_spec.rb
@@ -1,20 +1,26 @@
 require 'rails_helper'
 
 feature 'Meals' do
-  let(:recipe) { FactoryBot.create :recipe }
+  let(:rwi) { FactoryBot.create :recipe_with_ingredients }
+
+  before do
+    sign_in(email: rwi.user.email, password: rwi.user.password)
+    visit root_path
+  end
 
   scenario 'adding a Meal' do
-    login_user
-    visit root_path
     find("#nav-bar > a[href='#{meals_path}']").click
     click_link 'New Meal'
     fill_in 'Name', with: title
     fill_in 'Portions', with: 6
-    select(recipe.title.to_s, from: 'recipe-name').select_option
+    select(rwi.title.to_s, from: 'recipe-title').select_option
     click_button 'Create Meal'
     expect(page).to have_content 'Meal was successfully created'
     expect(page).to have_content 'Name: Meal Name'
     expect(page).to have_content 'Portions: 6'
     expect(page).to have_content 'Price per Portion: £20.0'
+    rwi.ingredients.each do |i|
+      expect(page).to have_content i.name.to_s
+    end
   end
 end
