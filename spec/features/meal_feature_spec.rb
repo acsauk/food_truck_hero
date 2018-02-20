efcaab4095563305676428d6dbb9c211da1d4f34
Alex Saunders <acsauk@gmail.com>
2018-02-20 22:07:00 +0000

    creates and passes editing meal from meal show, removes text from meal form so it becomes dynamic for create and update

--- spec/features/meal_feature_spec.rb
+++ spec/features/meal_feature_spec.rb
@@ -29,4 +29,21 @@ feature 'Meals' do
       expect(page).to have_content i.name.to_s
     end
   end
+
+  scenario 'editing meal - via meal#show' do
+    create_meal(recipe_title: rwi.title.to_s)
+    rwi2 = FactoryBot.create :recipe_with_ingredients
+    meal = Meal.last
+    visit meal_path meal
+    click_link 'Edit'
+    expect(page).to have_current_path(edit_meal_path(meal))
+    fill_in 'Name', with: "#{meal.name} edit"
+    fill_in 'Portions', with: "#{meal.portions} edit"
+    fill_in 'Price per portion', with: "#{meal.price_per_portion} edit"
+    select(rwi2.title, from: 'recipe-title').select_option
+    click_button 'Update Meal'
+    expect(page).to have_content 'Name: Meal name'
+    expect(page).to have_content 'Portions: 6'
+    expect(page).to have_content 'Price per portion: £3.5'
+  end
 end
