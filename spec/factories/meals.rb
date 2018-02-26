5ecf1d70386ffbb900fa95b8ed7a3a0690f7afae
Alex Saunders <acsauk@gmail.com>
2018-02-26 23:12:00 +0000

    creates failing spec for meal ingredients_cost virtual attribute

--- spec/factories/meals.rb
+++ spec/factories/meals.rb
@@ -1,7 +1,14 @@
 FactoryBot.define do
   factory :meal do
-    name "MyString"
+    sequence :name do |n|
+      "Name text #{n}"
+    end
     portions 1
     price_per_portion 1.5
+    # association :recipe
+
+    factory :meal_with_ingredients do
+      association :recipe_with_ingredients
+    end
   end
 end
--- spec/models/meal_spec.rb
+++ spec/models/meal_spec.rb
@@ -1,5 +1,6 @@
 require 'rails_helper'
 RSpec.describe Meal, type: :model do
+  let(:rwi) { FactoryBot.create :recipe_with_ingredients }
   let(:meal) { FactoryBot.create(:meal) }
 
   it 'has a valid factory' do
@@ -12,4 +13,9 @@ RSpec.describe Meal, type: :model do
   it { is_expected.to validate_presence_of(:name) }
   it { is_expected.to validate_presence_of(:portions) }
   it { is_expected.to validate_presence_of(:price_per_portion) }
+
+  it 'has the total cost of all ingredients associated with the meal' do
+    ingredients_price = meal.recipes.first.ingredients.first.product.price
+    it { is_expected.to have_attributes(ingredients_cost: ingredients_price) }
+  end
 end
