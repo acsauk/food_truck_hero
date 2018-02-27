69109f80953d14db84dc05264bac10ff19e85210
Alex Saunders <acsauk@gmail.com>
2018-02-27 20:08:22 +0000

    adds meal_with_recipe factory and slightly updates spec to properly request this

--- spec/factories/meals.rb
+++ spec/factories/meals.rb
@@ -5,10 +5,31 @@ FactoryBot.define do
     end
     portions 1
     price_per_portion 1.5
-    # association :recipe
 
-    factory :meal_with_ingredients do
-      association :recipe_with_ingredients
+    transient do
+      recipes_count 2
+      title 'Cooked Ham'
+      instructions 'Cook that ham good'
+    end
+
+    factory :meal_with_recipes do
+      after(:create) do |meal, evaluator|
+        (0...evaluator.recipes_count).each do
+          case evaluator.title
+          when nil
+            meal.recipeLists <<
+              FactoryBot.build(
+                :recipeList, recipe: FactoryBot.build(:recipe)
+              )
+          else
+            meal.recipeLists <<
+              FactoryBot.build(
+                :recipeList, recipe:
+                  FactoryBot.build(:recipe, title: evaluator.title)
+              )
+          end
+        end
+      end
     end
   end
 end
--- spec/models/meal_spec.rb
+++ spec/models/meal_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 RSpec.describe Meal, type: :model do
-  let(:rwi) { FactoryBot.create :recipe_with_ingredients }
   let(:meal) { FactoryBot.create(:meal) }
+  let(:meal_with_recipes) { FactoryBot.create(:meal_with_recipes) }
 
   it 'has a valid factory' do
     expect(meal).to be_valid
@@ -15,7 +15,7 @@ RSpec.describe Meal, type: :model do
   it { is_expected.to validate_presence_of(:price_per_portion) }
 
   it 'has the total cost of all ingredients associated with the meal' do
-    ingredients_price = meal.recipes.first.ingredients.first.product.price
+    ingredients_price = meal_with_recipes.recipes.first.ingredients.first.product.price
     it { is_expected.to have_attributes(ingredients_cost: ingredients_price) }
   end
 end
