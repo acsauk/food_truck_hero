1caab2c6fa15b015138b72e13060319ac6f14cb9
Alex Saunders <acsauk@gmail.com>
2018-04-09 22:09:19 +0100

    creates failing feature test for calculating meal margin

--- spec/features/meal_feature_spec.rb
+++ spec/features/meal_feature_spec.rb
@@ -107,4 +107,11 @@ feature 'Meals' do
     visit meal_path meal
     expect(page).to have_content "Cost per portion: £#{meal.cost_per_portion}"
   end
+
+  scenario 'viewing the margin of the meal' do
+    create_meal(recipe_title: rwi.title.to_s)
+    meal = Meal.last
+    visit meal_path meal
+    expect(page).to have_content "Margin: #{meal.margin}%"
+  end
 end
