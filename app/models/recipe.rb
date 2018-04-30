3921e207a3e9e8a510a0274c543013213b82cde2
Alex Saunders <acsauk@gmail.com>
2018-04-30 21:20:40 +0100

    updates recipes_controller spec to use correct attributes and sign user in

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -5,7 +5,7 @@ class Recipe < ApplicationRecord
   has_many :ingredientLists, dependent: :destroy
   has_many :ingredients, through: :ingredientLists
 
-  belongs_to :user
+  belongs_to :user, optional: true
 
   accepts_nested_attributes_for :ingredientLists
 
